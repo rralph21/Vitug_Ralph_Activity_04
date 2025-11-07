059a43979ee9b856b9073e6bbe1fc310b4d25b6e
Ralph.Vitug <rralph@rrc.ca>
2025-11-07 13:08:58 -0600

    feat: __on_add_contact method ,slot and signal

--- contact_list/contact_list.py
+++ contact_list/contact_list.py
@@ -5,6 +5,8 @@ __version__ = "1.0.0"
 __credits__ = ""
 
 from PySide6.QtWidgets import  QMainWindow, QLineEdit, QPushButton, QTableWidget, QLabel, QVBoxLayout, QWidget, QTableWidgetItem
+from PySide6.QtWidgets import QMessageBox
+from PySide6.QtCore import Slot, Signal
 
 class ContactList(QMainWindow):
     """Represents a window that provides the UI to manage contacts."""
@@ -48,3 +50,33 @@ class ContactList(QMainWindow):
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
+
+
+    @Slot
+    def __on_add_contact(self) -> None:
+        """
+        on_add_contact (None): method will be a slot for the signal that occurs
+        when the user clicks the Add Contact Button.
+        """
+
+        name = self.contact_name_input.text().strip()
+
+        phone = self.phone_input.text().strip()
+
+        if name and phone:
+           row_position = self.contact_table.rowCount()
+           self.contact_table.insertRow(row_position)
+
+           name_item = QTableWidgetItem(name)
+           phone_item = QTableWidgetItem(phone)
+
+           self.contact_table.setItem(row_position, 0, name_item)
+           self.contact_table.setItem(row_position, 1, phone_item)
+
+           self.status_label.setText(f"Added contact: {name}")
+
+        else:
+            self.status_label.setText(
+                "Plese enter a contact name and phone number")
+        
+
